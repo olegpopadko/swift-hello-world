b9169d76dbf628edbe6834f4d1424e0e3f76bbcb
Oleg Popadko <oleg.popadko@icloud.com>
2014-09-20 19:45:20 +0300

    Initial Commit

---
+++ QuickSort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  QuickSort
+//
+//  Created by Oleg Popadko on 9/20/14.
+//  Copyright (c) 2014 Oleg Popadko. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
